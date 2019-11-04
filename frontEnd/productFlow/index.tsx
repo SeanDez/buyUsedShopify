990e39a2984dda3a81654d9f0e270d096f98063d
Sean Dez <sd@seandezoysa.com>
2019-11-04 20:00:40 +0700

    wip set text field

--- frontEnd/productFlow/index.tsx
+++ frontEnd/productFlow/index.tsx
@@ -6,18 +6,39 @@ import Typography from "@material-ui/core/Typography";
 import Input from "@material-ui/core/Input";
 import Button from "@material-ui/core/Button";
 import FormControl from "@material-ui/core/FormControl";
+import TextField from "@material-ui/core/TextField";
+
+////// Component Functions //////
+
+/** Fires a modal blindly. No control logic
+ *
+ * todo: write the presentation logic now. Figure out the control logic later (will go in a back end function that then updates a variable set monitor via useEffect
+ */
+const renderModal = () => {
+
+};
+
+
+////// Component //////
 
 const ProductFlowContainer = props => {
   ////// Component State //////
+
+  // display settings
+  const [displaySuccessModal, setDisplaySuccessModal] = useState(true);
+
+
+  // product price and quantity
   const [tradeInPrice, setTradeInPrice] = useState<number|undefined>(undefined);
   const [quantity, setQuantity] = useState<number>(1);
 
   useEffect(() => setTradeInPrice(12.99),[tradeInPrice]);
 
 
+  ////// Render //////
   if (tradeInPrice) {
     return (
-    <form>
+    <FormContainer>
       {/*
        Add info
        quantity box
@@ -27,15 +48,17 @@ const ProductFlowContainer = props => {
 
 
       <InlineContainer>
-        <ThinInput
+        <ThinTextField
           value={quantity}
+          onChange={e => setQuantity(e.target.value)}
         />
-        <div id="tradeInFormButtons">
+        <ButtonContainer>
           <Button size="small">Add to Trade Ins</Button>
           <Button size="small">View</Button>
-        </div>
+        </ButtonContainer>
       </InlineContainer>
-    </form>
+
+    </FormContainer>
   )}
 
   return (
@@ -46,18 +69,42 @@ const ProductFlowContainer = props => {
 };
 
 
-const InlineContainer = styled.div`
+
+////// Component Styles //////
+
+const FormContainer = styled.form`
   display: flex;
-  
-  &>* {
-    margin-right: "40px";
+  flex-flow: column nowrap;
+  align-items: center;
+  max-width: 300px;
+
+  > * {
+    margin-top: 10px;
   }
 `;
 
-const ThinInput = styled(Input)`
+const InlineContainer = styled.div`
+  display: flex;
+  
+  > * { // direct children
+    margin-right: 10px;
+    //border: 2px dashed blueviolet;
+  }
+`;
+
+const ThinTextField = styled(TextField)`
   display: inline;
-  width: 40px;
-  text-align: center;
+  width: 30px;
+  margin-right: 10px !important;
+  text-align: center !important;
+`;
+
+const ButtonContainer = styled.div`
+
+  > * {
+    margin-right: 10px !important;
+    border: 2px dashed orange !important;
+  }
 `;
 
 
